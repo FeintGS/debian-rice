7648893840ab5995951f5e23ace2fd07876e3dff
FeintGS <feintgs@gmail.com>
2019-01-31 20:54:27 -0500

    SUPER DUPER IMPORTANT CONKY BACKUP

--- conky/launch_all.sh
+++ conky/launch_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 pkill conky
 
-conky -dq -c $HOME/.config/conky/syspanel_rc
-#conky -dq -c $HOME/.config/conky/system_rc
+#conky -dq -c $HOME/.config/conky/syspanel_rc
+conky -dq -c $HOME/.config/conky/system_rc
 #conky -dq -c $HOME/.config/conky/processes_rc
 #conky -dq -c $HOME/.config/conky/todolist_rc
 #conky -dq -c $HOME/.config/conky/time_rc
