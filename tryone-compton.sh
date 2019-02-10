c0825125e2a11cb7ecf0b1f783a5be794d0c4f14
FeintGS <feint@debian>
2019-02-09 19:16:06 -0500

    Update script for installing compton fork

--- tryone-compton.sh
+++ tryone-compton.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 # dependencies listed in repo README
-sudo apt --fix-broken install libx11-dev libxcomposite-dev libxdamage-dev libxfixes-dev libxrender-dev libxrandr-dev libxinerama-dev pkg-config make 
+sudo apt install libx11-dev libxcomposite-dev libxdamage-dev libxfixes-dev libxrender-dev libxrandr-dev libxinerama-dev pkg-config make 
 
 # OpenGL libraries and headers
-sudo apt-get install --fix-missing libglu1-mesa-dev freeglut3-dev mesa-common-dev
+sudo apt-get install libglu1-mesa-dev freeglut3-dev mesa-common-dev
