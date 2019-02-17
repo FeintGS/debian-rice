c39dd76ac37ef592d75002417be5d597b2b9508e
FeintGS <feintgs@gmail.com>
2019-02-17 16:39:58 -0500

    For now, let's delete the install scripts

--- tryone-compton.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# dependencies listed in repo README
-sudo apt install libx11-dev libxcomposite-dev libxdamage-dev libxfixes-dev libxrender-dev libxrandr-dev libxinerama-dev pkg-config make 
-
-# OpenGL libraries and headers
-sudo apt-get install libglu1-mesa-dev freeglut3-dev mesa-common-dev
